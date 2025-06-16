a747fe2ca548ef2fd50d90085df5fc7ca844399c
Zeynep Köse <zeynep.j17@gmail.com>
2025-06-17 02:18:54 +0300

    update

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Component from "./poem-card"
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Component />
+    </main>
+  )
+}
